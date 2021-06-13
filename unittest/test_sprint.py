becf53ffe923f3aa3e94609e7751e7645dfd1559
tungyuehlin <tungyuehlin@gmail.com>
2021-06-13 16:59:29 +0800

    unittest: fix test_sprint timzone problem

--- unittest/test_sprint.py
+++ unittest/test_sprint.py
@@ -1,10 +1,13 @@
-from jiraworklog.sprint import Sprint
+from jiraworklog.sprint import Sprint, TIMESTAMP
 
 
 def test_start_time():
     start_date = '31/May/21 3:12 PM'
-    assert 1622401920.0 == Sprint({'startDate': start_date}).start_time
+    start_time = Sprint({'startDate': start_date}).start_time
+    assert isinstance(start_time, TIMESTAMP)
+
 
 def test_end_time():
     end_date = '11/Jun/21 7:12 AM'
-    assert 1623366720.0 == Sprint({'endDate': end_date}).end_time
+    end_time = Sprint({'endDate': end_date}).end_time
+    assert isinstance(end_time, TIMESTAMP)
